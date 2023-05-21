0091d12fd4ed24e6aa60a6920cfa3cd0a1f0b67b
YILMAZ ER <developer.yilmazer@gmail.com>
2023-05-21 14:01:08 +0300

    Initial Commit

---
+++ DarkMode/DarkModeApp.swift
@@ -0,0 +1,17 @@
+//
+//  DarkModeApp.swift
+//  DarkMode
+//
+//  Created by YILMAZ ER on 21.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct DarkModeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
